c42eef8b32d1d10c21143d3ca36998b7b695031d
aahad91 <abdulahadayaz@hotmail.com>
2019-10-29 16:04:17 +0100

    added webui(not ready) and troubleshooting mongodb connection with hss

--- base/install.sh
+++ base/install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-chmod 666 setup.sh
-./setup.sh
-./usr/bin/open5gs-pcrfd
-./usr/bin/open5gs-pgwd
-./usr/bin/open5gs-sgwd
-./usr/bin/open5gs-hssd
-./usr/bin/open5gs-mmed
+chmod +x setup.sh
+chmod +x /dev/net/tun
+/setup.sh
+/usr/bin/open5gs-pcrfd
+/usr/bin/open5gs-pgwd
+/usr/bin/open5gs-sgwd
+/usr/bin/open5gs-hssd
+/usr/bin/open5gs-mmed
