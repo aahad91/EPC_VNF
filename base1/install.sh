2aa1c1affc4c7e1ffa1aa15a9e04916a4e2d6b49
aahad91 <abdulahadayaz@hotmail.com>
2019-11-07 16:55:06 +0100

    modification in iptables rules and added test-serve(iperf) for end to end testing

--- base1/install.sh
+++ base1/install.sh
@@ -3,5 +3,7 @@
 chmod +x setup.sh
 chmod +x /dev/net/tun
 /setup.sh
-iptables -t nat -A POSTROUTING -s 45.45.0.0/16 ! -o ogstun -j MASQUERADE
+#iptables -t nat -A POSTROUTING -s 45.45.0.0/16 ! -o ogstun -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -I INPUT -i ogstun -j ACCEPT
 /usr/bin/open5gs-pcrfd -D; /usr/bin/open5gs-pgwd -D; /usr/bin/open5gs-sgwd -D; /usr/bin/open5gs-hssd -D; /usr/bin/open5gs-mmed
