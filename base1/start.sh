4935bb3ccaed237a1212ca708913cf8ffe931a11
aahad91 <abdulahadayaz@hotmail.com>
2019-11-21 02:24:49 +0100

    error fixes: UE is getting ip and iperf is working

--- base1/start.sh
+++ base1/start.sh
@@ -6,4 +6,4 @@ chmod +x /dev/net/tun
 #iptables -t nat -A POSTROUTING -s 45.45.0.0/16 ! -o ogstun -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -I INPUT -i ogstun -j ACCEPT
-/usr/local/bin/open5gs-pcrfd -D; /usr/local/bin/open5gs-pgwd -D; /usr/local/bin/open5gs-sgwd -D; /usr/local/bin/open5gs-hssd -D; /usr/local/bin/open5gs-mmed
+/usr/bin/open5gs-pcrfd -D; /usr/bin/open5gs-pgwd -D; /usr/bin/open5gs-sgwd -D; /usr/bin/open5gs-hssd -D; /usr/bin/open5gs-mmed
