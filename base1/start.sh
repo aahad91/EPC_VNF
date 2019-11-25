1091e0fadd36520b55e669c6c8cab540890bcecc
aahad91 <abdulahadayaz@hotmail.com>
2019-11-25 01:11:23 +0100

    mongdb error resolved / epc not getting the IPs

--- base1/start.sh
+++ base1/start.sh
@@ -8,6 +8,7 @@ chmod +x /dev/net/tun
 #iptables -t nat -A POSTROUTING -s 45.45.0.0/16 ! -o ogstun -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -I INPUT -i ogstun -j ACCEPT
+sleep 20
 /usr/bin/open5gs-pcrfd -D; /usr/bin/open5gs-pgwd -D; /usr/bin/open5gs-sgwd -D; /usr/bin/open5gs-hssd -D; /usr/bin/open5gs-mmed
 
 echo "EPC VNF started"
--- mongodb/start.sh
+++ mongodb/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 date > /tngbench_share/start.txt
-#mongod --bind_ip 192.168.24.50
-./startdb.sh &
+mongod
 ./loadimsi.sh
 #mongoimport --db open5gs --collection subscribers --file imsi1.json --type json --jsonArray
-sleep infinity
