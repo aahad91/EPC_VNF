d49675440f9e3deed42450a66e06c1405c259724
aahad91 <abdulahadayaz@hotmail.com>
2019-12-13 14:08:25 +0100

    successfully run 64 experiments

--- srsLTE/run_ue.sh
+++ srsLTE/run_ue.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 sleep 20
-srsue /config/ue.conf.fauxrf --usim.imsi=001010000000001 --usim.k=c8eba87c1074edd06885cb0486718341 --usim.algo=milenage --usim.opc=17b6c0157895bcaa1efc1cef55033f5f --nas.apn=internet
+srsue /config/ue.conf.fauxrf --usim.imsi=001010000000001 --usim.k=c8eba87c1074edd06885cb0486718341 --usim.algo=milenage --usim.opc=17b6c0157895bcaa1efc1cef55033f5f --nas.apn=internet &
+sleep 240
+echo "UE VNF started" >> /tngbench_share/start.txt
+ip r add 192.168.30.0/24 via 45.45.0.2
+iperf -c 192.168.30.10 -w 32M -yC >> /tngbench_share/iperf.csv
+#iperf output format
+#timestamp,source_address,source_port,destination_address,destination_port,interval,transferred_bytes,bits_per_second
--- srsLTE/start_ue.sh
+++ srsLTE/start_ue.sh
@@ -2,7 +2,6 @@
 date > /tngbench_share/start.txt
 sysctl net.ipv6.conf.all.disable_ipv6=0
 sysctl net.ipv4.conf.all.forwarding=1
-./ip_rule.sh &
-sleep 60
+sleep 20
 ./run_ue.sh &
 echo "UE VNF started"
