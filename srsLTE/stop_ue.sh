811d9a68b372f8cf794cd66d24c935ba9f3a8c0b
aahad91 <abdulahadayaz@hotmail.com>
2019-12-15 05:18:29 +0100

    fix iperf result collection error

--- srsLTE/stop_ue.sh
+++ srsLTE/stop_ue.sh
@@ -5,5 +5,5 @@ cp /tmp/ue_metrics.csv /tngbench_share/
 sleep 1
 python ./log_intf_statistics.py /tngbench_share/result.yml
 sleep 1
-python ./process_iperf3_results.py /tngbench_share/iperf.json /tngbench_share/result.yaml
+python ./process_iperf3_results.py /tngbench_share/iperf.json /tngbench_share/result.yml
 date > /tngbench_share/stop.txt
