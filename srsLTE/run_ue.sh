0e93346b7dee340b16acb61f7ef1929fb71a4897
aahad91 <abdulahadayaz@hotmail.com>
2020-01-13 16:57:35 +0100

    iperf3 time limit set to 60s to get variation in sent bits/bytes

--- srsLTE/run_ue.sh
+++ srsLTE/run_ue.sh
@@ -5,7 +5,7 @@ sleep 240
 echo "UE VNF started" >> /tngbench_share/start.txt
 ip r add 192.168.30.0/24 via 45.45.0.2
 echo "ip rule added" >> /tngbench_share/start.txt
-iperf3 -c 192.168.30.10 -w 32M -J >> /tngbench_share/iperf.json
+iperf3 -c 192.168.30.10 -w 32M -t 60 -J >> /tngbench_share/iperf.json
 echo "iperf executed" >> /tngbench_share/start.txt
 #iperf output format
 #timestamp,source_address,source_port,destination_address,destination_port,interval,transferred_bytes,bits_per_second
